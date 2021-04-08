db1e991c496ba9da37ea90519ceaeb644b79d2fe
Jose Andres <Gorusuke123@gmail.com>
2021-04-08 12:15:33 -0500

    Agregando react-router-dom para las rutas de nuestra aplicacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Components/pages/Login";
+import SignUp from "./Components/pages/SignUp";
+import Chat from "./Components/pages/Chat";
+
+// import { Protected } from "./component/routes/Protected";
+// import { Public } from "./component/routes/Public";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo!!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/new-account" component={SignUp} />
+
+        <Route exact path="/" component={Chat} />
+      </Switch>
+    </Router>
   );
 }
 
